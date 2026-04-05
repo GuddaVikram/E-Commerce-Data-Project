5c1a7442d4eff9a431e51750253f47ca499b5593
GuddaVikram <guddavikram@gmail.com>
2026-04-05 22:11:05 +0530

    daily_revenue_metric

--- ecommerce_dbt/models/marts/core/fact_orders.sql
+++ ecommerce_dbt/models/marts/core/fact_orders.sql
@@ -4,16 +4,16 @@ select
     o.id          as order_id,
     c.customer_sk,
     o.status      as order_status,
-    o.purchase_ts,
-    o.approved_ts,
-    o.delivered_ts,
-    o.estimated_delivery_ts
+    o.purchased_at,
+    o.approved_at,
+    o.delivered_at,
+    o.estimated_delivery_at
 from {{ ref('stg_orders') }} o
 join {{ ref('dim_customers') }} c
   on o.customer_id = c.customer_id
   
 
 {% if is_incremental() %}
-where o.purchase_ts >
-      (select max(purchase_ts) from {{ this }})
+where o.purchased_at >
+      (select max(purchased_at) from {{ this }})
 {% endif %}
---
+++ ecommerce_dbt/models/marts/metrics/daily_revenue.sql
@@ -0,0 +1,18 @@
+{{ config(materialized='table') }}
+
+select
+    date_trunc('day', o.purchased_at) as purchase_date,
+    sum(oi.price) as total_revenue,
+    count(distinct o.order_id) as total_orders,
+    count(*) as total_items_sold,
+    count(distinct o.customer_sk) as unique_customers,
+    count(distinct oi.product_sk) as unique_products_sold,
+    sum(oi.freight_value) as total_freight_revenue,
+    round(sum(oi.price) / count(distinct o.order_id),2) as avg_order_value
+    
+from {{ ref('fact_orders') }} o
+join {{ ref('fact_order_items') }} oi
+  on o.order_id = oi.order_id
+where o.order_status = 'delivered'
+group by 1
+order by 1
